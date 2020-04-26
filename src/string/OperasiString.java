e6fc68571c50fa94f5ed5fa3c157ef03a5e63a09
Andhika Prima Jaya <andhika644@gmail.com>
2020-04-26 19:55:26 +0800

    toUpperCase untuk membuat penulisan abjad menjadi kapital semua. toLowerCase untuk membuat penulisan abjad menjadi kecil semua. substring(5) untuk mengambil nilai value dari sebuah string dan menampilkan value tsb mulai index 5(Awal index 0) sampai dgn akhir sebuah value string. substring(0,5)) untuk mengambil nilai value dari sebuah string dan menampilkan value tsb mulai index 0 sampai <5.

--- src/string/OperasiString.java
+++ src/string/OperasiString.java
@@ -9,5 +9,10 @@ public class OperasiString {
         char[] uniskaChar = {'U','N','I','S','K','A'};
         String uniskaString = new String(uniskaChar);
         System.out.println(uniskaString);
+
+        System.out.println(kota.toUpperCase()); //membuat penulisan abjad menjadi kapital semua
+        System.out.println(kota.toLowerCase()); //membuat penulisan abjad menjadi kecil
+        System.out.println(kota.substring(5)); //mengambil nilai value dari sebuah string dan menampilkan value tsb mulai index 5(Awal index 0) sampai dgn akhir sebuah value string.
+        System.out.println(kota.substring(0,5)); //mengambil nilai value dari sebuah string dan menampilkan value tsb mulai index 0 sampai <5.
     }
 }
