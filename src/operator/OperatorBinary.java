d7f1f0b0640279e3ccbe39cdd096df0fb8acb258
Andhika Prima Jaya <andhika644@gmail.com>
2020-04-26 20:13:37 +0800

    Operator Binary Aritmatika

--- src/operator/OperatorBinary.java
+++ src/operator/OperatorBinary.java
@@ -1,7 +1,7 @@
 package operator;
 
 public class OperatorBinary {
-    public static void main(String[] args) {
+
         //operator (=) sebagai assignment/penugasan
         //menempatkan nilai kedalam variabel
         //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
@@ -10,5 +10,13 @@ public class OperatorBinary {
         String nama                     = "Andhika Prima Jaya";
         int jumlahSKS                   = 24;
         double indeksPrestasiKumulatif  = 3.90;
+
+        public static void main(String[] args) {
+            System.out.println(9+3);
+            System.out.println(9-3);
+            System.out.println(9*3);
+            System.out.println(9/3);
+            System.out.println(9%3);
+            System.out.println(9%2);
     }
 }
