2772a6a094ceee150fcca4788aa84b559e717862
Andhika Prima Jaya <andhika644@gmail.com>
2020-04-26 20:48:16 +0800

    Operator Equality dan Relational

---
+++ src/operator/OperatorEquaility.java
@@ -0,0 +1,19 @@
+package operator;
+
+public class OperatorEquaility {
+    public static void main(String[] args) {
+        int i = 5;
+        int j = 6;
+
+        //Output berupa True / False
+        System.out.println(i==5); // i sama dengan 5 jer. Bujur
+        System.out.println(i==6); // j sama dengan 6. salah jeh
+        System.out.println(i==j); // i sama dengan j. luput am
+        System.out.println(i>j); // i lebih dari j. luput banar
+        System.out.println(i<j); // i kurang dari j. nah, hanyar bujur ini
+        System.out.println(5>6); // 5 lebih dari 6. kadanya mungkin
+        System.out.println(5<6); // 5 kurang dari 6. nyata haja om ai
+        System.out.println(i!=5); // i tidak sama dengan 5. nah maigau nih
+        System.out.println(j!=5); // j tidak sama dengan 5. akhirnya utuh sadar sdh wkwk
+    }
+}
