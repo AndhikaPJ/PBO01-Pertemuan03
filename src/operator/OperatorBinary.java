792060d2bb1ad504a787da70b110e764c1f1e529
Andhika Prima Jaya <andhika644@gmail.com>
2020-04-26 20:11:28 +0800

    Operator Binary Assignment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,14 @@
+package operator;
+
+public class OperatorBinary {
+    public static void main(String[] args) {
+        //operator (=) sebagai assignment/penugasan
+        //menempatkan nilai kedalam variabel
+        //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+
+        String nama                     = "Andhika Prima Jaya";
+        int jumlahSKS                   = 24;
+        double indeksPrestasiKumulatif  = 3.90;
+    }
+}
