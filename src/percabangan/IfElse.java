ecc04ed09b16e548bf0e34bbdcf99af63ac063c4
Andhika Prima Jaya <andhika644@gmail.com>
2020-04-26 20:55:22 +0800

    Percabangan IfElse, False Condition

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -2,10 +2,10 @@ package percabangan;
 
 public class IfElse {
     public static void main(String[] args) {
-        double totalBelanja = 53000;
-        double uangDiDompet = 25000;
+        double totalBelanja = 25000;
+        double uangDiDompet = 50000;
 
-        if (uangDiDompet<totalBelanja) {
+        if (uangDiDompet < totalBelanja) {
             System.out.println("Nah duitnya kada mayu, kurangi belanjaanya!");
         } else {
             System.out.println("Alhamdulillah, belabih bawa duit sekalinya. Happy mukbang!");
