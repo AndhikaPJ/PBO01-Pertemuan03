74da3ee083682ec50bd2a836120c99f11503bbda
Andhika Prima Jaya <andhika644@gmail.com>
2020-04-26 20:53:33 +0800

    Percabangan IfElse

---
+++ src/percabangan/IfElse.java
@@ -0,0 +1,14 @@
+package percabangan;
+
+public class IfElse {
+    public static void main(String[] args) {
+        double totalBelanja = 53000;
+        double uangDiDompet = 25000;
+
+        if (uangDiDompet<totalBelanja) {
+            System.out.println("Nah duitnya kada mayu, kurangi belanjaanya!");
+        } else {
+            System.out.println("Alhamdulillah, belabih bawa duit sekalinya. Happy mukbang!");
+        }
+    }
+}
