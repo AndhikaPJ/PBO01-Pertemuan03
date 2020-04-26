d01aa58a1d6cbc40a73cdb3fab935135de74e237
Andhika Prima Jaya <andhika644@gmail.com>
2020-04-26 20:40:30 +0800

    Operator Unary :D

--- src/operator/OperatorUnary.java
+++ src/operator/OperatorUnary.java
@@ -9,5 +9,22 @@ public class OperatorUnary {
         System.out.println(i);   // print i dgn nilai 2
         i=i+1;                  // karena nilai i sebelumnya 2, maka i = 2 + 1 = 3 , atau sama saja dengan i++ = i=i+1,
         System.out.println(i); // print i dgn nilai 3
+
+        //============================================//
+        // berlaku jua jer gsn penambahan angka dan operator aritmatika lainnya.
+        /// ini kita melanjut nilai variabel i yg sebelumnya 3.
+        System.out.println("==========================");
+        i+=3; // sama haja lawan i = i + 3, i = 3 + 3
+        System.out.println(i);
+
+        i+=3;
+        System.out.println(i);
+
+        i*=3; // ini sama haja jua lwn i = i * 3
+        System.out.println(i);
+
+        i*=3;
+        System.out.println(i);
+
     }
 }
